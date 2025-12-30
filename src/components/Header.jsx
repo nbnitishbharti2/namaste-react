f8cbfceff7fb36757d283a3c9db853802a798ddc
nbnitishbharti2 <nbnitishbharti2@gmail.com>
2025-12-30 21:35:03 +0530

    Episode-09 Optimizing our App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 const Header = () => {
   // Never create useState outside of Functional component
   // Its used to create local variable for Functional component
   // Always use useState hook on top of your Functional component
   // Never use useState inside of if else, loop & Functions
-  // 
-  const [btnName, setBtnName] = useState('Login');
+  //
+  const [btnName, setBtnName] = useState("Login");
 
   // If no dependency array means useEffect is called on every render
   // If dependency array is empty = [] => useEffect is called only once on the initial render
   //  If dependency array is [btnName] => called everytime btnName is updayed or changes
   useEffect(() => {
-    console.log('useEffect called');
-    
+    console.log("useEffect called");
   }, [btnName]);
-  console.log('Header called');
+  console.log("Header called");
 
   return (
     <div className="header">
@@ -25,14 +24,25 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About US</Link></li>
-          <li><Link to="/contact">Contact US</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About US</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact US</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login-button"
             onClick={() => {
-              setBtnName(prevBtnName => prevBtnName === "Login" ? "Logout" : "Login");
+              setBtnName((prevBtnName) =>
+                prevBtnName === "Login" ? "Logout" : "Login"
+              );
             }}
           >
             {btnName}
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,45 +1,31 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import Shimmer from "./Shimmer";
-import { MENU_URL } from "../utils/constants";
+
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-	const [resInfo, setResInfo] = useState(null);
-	const { resId } = useParams();
+  const { resId } = useParams();
+  const resInfo = useRestaurantMenu(resId);
 
-	useEffect(() => {
-		fetchRestaurantMenu();
-	}, []);
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+  console.log(resInfo);
 
-	const fetchRestaurantMenu = async () => {
-		const data = await fetch(MENU_URL + resId);
-		const json = await data.json();
-		setResInfo(json?.data);
-	}
-
-	if (resInfo === null) {
-		return <Shimmer />;
-	}
-	console.log(resInfo);
-	const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
-	const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
-	console.log(itemCards);
-
-	return (
-		<div className="menu">
-			<h1>{name}</h1>
-			<h2>{cuisines.join(", ")}</h2>
-			<h3>{costForTwoMessage}</h3>
-			<h2>Menu</h2>
-			<ul>
-				{itemCards.map((item, index) => (
-					<li key={index}>
-						{item?.card?.info?.name} - {item?.card?.info?.defaultPrice / 100}
-					</li>
-				))}
-			</ul>
-		</div>
-	)
-}
+  return (
+    <div className="menu">
+      <h1>{resInfo?.[0]?.restaurantName}</h1>
+      <h2>Menu</h2>
+      <ul>
+        {resInfo.map((item) => (
+          <li key={item.itemID}>
+            {item?.itemName} - {item?.itemPrice}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default RestaurantMenu;
